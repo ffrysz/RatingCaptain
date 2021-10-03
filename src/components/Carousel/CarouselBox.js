c3f1622e2bb35ec35f9863d9828195b2467c8662
Filip Frysz <ffrysz@gmail.com>
2021-10-03 18:56:12 +0200

    Add Carousel

---
+++ src/components/Carousel/CarouselBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CarouselBox = () => {
+
+  return (
+    
+  );
+}
+
+export default CarouselBox;
--- src/components/Carousel/CarouselContainer.js
+++ src/components/Carousel/CarouselContainer.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Carousel } from 'react-responsive-carousel';
+import styles from './CarouselContainer.scss';
 
-const CarouselContainer = ({ products }) => (
-  <h1>{products.last_page_url}</h1>
-);
+const CarouselContainer = ({ products }) => {
+
+  if (products.data) {
+    return (
+      <div className={styles.carouselBox}>
+        <Carousel showArrows={true} autoPlay={true} interval={1000}>
+          <div>
+            <img src={products.data[0].main_image} />
+            <p className="legend">Legend 1</p>
+          </div>
+          <div>
+            <img src={products.data[1].main_image} />
+            <p className="legend">Legend 2</p>
+          </div>
+          <div>
+            <img src={products.data[2].main_image} />
+            <p className="legend">Legend 3</p>
+          </div>
+        </Carousel>
+      </div>
+    );
+  } else return (<h1>Waiting for the data from server</h1>);
+
+}
 
 export default CarouselContainer;
